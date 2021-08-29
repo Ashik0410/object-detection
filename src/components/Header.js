47ccdb17ee9d56555bd116476196304ce49bb902
Ashik0410 <hameedashik100@gmail.com>
2021-08-29 16:52:42 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,17 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Dropdown from 'react-bootstrap/Dropdown'
+import {Navbar, Container, FormControl} from 'react-bootstrap';
+// import Dropdown from 'react-bootstrap/Dropdown'
 const Header = () => {
 	return(
-		<Container>
-		<Navbar>Ashik</Navbar>
-		<Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
-    Dropdown Button
-  </Dropdown.Toggle>
-
-  <Dropdown.Menu>
-    <Dropdown.Item href="https://shopping-cart-by-ashik.netlify.app/" target="_blank">shoe</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">bag</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">shirt</Dropdown.Item>
-  </Dropdown.Menu>
-</Dropdown>
-		</Container>
+		<Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand>
+    <a> Shopping Cart </a>
+    </Navbar.Brand>
+    <Navbar.Text className="search">
+    <FormControl style={{widht : 500}} placeHolder='search a product'/>
+    </Navbar.Text>
+    </Container>
+    </Navbar>
 
 		);
 }
