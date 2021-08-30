f270e934551edf385422f84740c23a3b39c9a3f0
Ashik0410 <hameedashik100@gmail.com>
2021-08-30 11:06:15 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-import {Navbar, Container, FormControl} from 'react-bootstrap';
-// import Dropdown from 'react-bootstrap/Dropdown'
+import {Navbar, Container, FormControl, Dropdown, Nav} from 'react-bootstrap';
+import { FaShoppingCart } from 'react-icons/fa';
+
 const Header = () => {
 	return(
 		<Navbar bg="dark" variant="dark">
@@ -7,9 +8,20 @@ const Header = () => {
     <Navbar.Brand>
     <a href="/"> Shopping Cart </a>
     </Navbar.Brand>
-    <Navbar.Text className="search">
-    <FormControl style={{widht : 500}} placeHolder='search a product'/>
+    <Navbar.Text className="search">  
+    <FormControl style={{width : 500}} placeHolder='search a product'/>
     </Navbar.Text>
+    <Nav>
+      <Dropdown alignRight>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+  <FaShoppingCart color="white" fontSize="25px"/>
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu style={{minWidth: 370}}>
+  <span style={{padding :10}}> Cart is empty </span>
+  </Dropdown.Menu>
+</Dropdown>
+    </Nav>
     </Container>
     </Navbar>
 
